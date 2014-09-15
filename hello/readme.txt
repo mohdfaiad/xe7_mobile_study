2e2a344b204233f31af63a3ca1b594f6f34808f5
wingfish <wingfish@gmail.com>
2014-09-15 13:01:20 +0800

    2014.9.15. function wifi setting under work

--- hello/readme.txt
+++ hello/readme.txt
@@ -1,2 +1,3 @@
+2014.9.15. re-make git, function wifi setting simulation still under work
 2014.9.14. tab setting, item wifi can show the next tableview, wifi view; need more work
 2014.9.14. first version, add button and label, add more listbox to test
--- hello/uHello.pas
+++ hello/uHello.pas
@@ -3,10 +3,11 @@ unit uHello;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.TabControl,
-  FMX.StdCtrls, FMX.Gestures, FMX.ExtCtrls, Data.Bind.EngExt, Fmx.Bind.DBEngExt,
-  System.Rtti, System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.Components,
+  FMX.StdCtrls, FMX.Gestures, FMX.ExtCtrls, Data.Bind.EngExt, FMX.Bind.DBEngExt,
+  System.Rtti, System.Bindings.Outputs, FMX.Bind.Editors, Data.Bind.Components,
   FMX.ListView.Types, FMX.ListView, FMX.ListBox, FMX.Layouts, System.Actions,
   FMX.ActnList;
 
@@ -46,17 +47,15 @@ type
     Label2: TLabel;
     Button1: TButton;
     ChangeTabActionSetting: TChangeTabAction;
-    ListBox1: TListBox;
+    ListBoxWifi: TListBox;
     ListBoxItem2: TListBoxItem;
-    ListBoxItem6: TListBoxItem;
-    Switch2: TSwitch;
-    ListBoxGroupHeader1: TListBoxGroupHeader;
-    ListBoxItem7: TListBoxItem;
+    SwitchWifi: TSwitch;
     procedure FormCreate(Sender: TObject);
     procedure FormGesture(Sender: TObject; const EventInfo: TGestureEventInfo;
       var Handled: Boolean);
     procedure btnHelloClick(Sender: TObject);
     procedure itemWifiClick(Sender: TObject);
+    procedure SwitchWifiSwitch(Sender: TObject);
   private
     { Private declarations }
   public
@@ -72,54 +71,85 @@ implementation
 {$R *.iPhone4in.fmx IOS}
 {$R *.LgXhdpiPh.fmx ANDROID}
 
-//2014.9.13.
-//点击按钮，显示hello
+// 2014.9.13.
+// 点击按钮，显示hello
 procedure TTabbedForm.btnHelloClick(Sender: TObject);
 begin
-  lblHello.Text:='Hello';
+  lblHello.Text := 'Hello';
 end;
 
-//2014.9.14.
+// 2014.9.14.
 procedure TTabbedForm.FormCreate(Sender: TObject);
 begin
   { This defines the default active tab at runtime }
   TabControl1.ActiveTab := TabItem1;
 
-  //set setting tab default
-  TabControlSetting.ActiveTab:=tabSetting;
+  // set setting tab default
+  TabControlSetting.ActiveTab := tabSetting;
 
-  //set property TabPosition to None, don't show the tab
-  tabcontrolSetting.TabPosition:=fmx.TabControl.TTabPosition.None;
+  // set property TabPosition to None, don't show the tab
+  TabControlSetting.TabPosition := FMX.TabControl.TTabPosition.None;
 end;
 
-//2014.9.13.
-//android可以用手势控制切换tab
+// 2014.9.13.
+// android可以用手势控制切换tab
 procedure TTabbedForm.FormGesture(Sender: TObject;
   const EventInfo: TGestureEventInfo; var Handled: Boolean);
 begin
 {$IFDEF ANDROID}
   case EventInfo.GestureID of
     sgiLeft:
-    begin
-      if TabControl1.ActiveTab <> TabControl1.Tabs[TabControl1.TabCount-1] then
-        TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex+1];
-      Handled := True;
-    end;
+      begin
+        if TabControl1.ActiveTab <> TabControl1.Tabs[TabControl1.TabCount - 1]
+        then
+          TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex + 1];
+        Handled := True;
+      end;
 
     sgiRight:
-    begin
-      if TabControl1.ActiveTab <> TabControl1.Tabs[0] then
-        TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex-1];
-      Handled := True;
-    end;
+      begin
+        if TabControl1.ActiveTab <> TabControl1.Tabs[0] then
+          TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex - 1];
+        Handled := True;
+      end;
   end;
 {$ENDIF}
 end;
 
-//2014.9.14.
+// 2014.9.14.
 procedure TTabbedForm.itemWifiClick(Sender: TObject);
 begin
   ChangeTabActionWifi.ExecuteTarget(self);
+end;
+
+// 2014.9.15.
+procedure TTabbedForm.SwitchWifiSwitch(Sender: TObject);
+var
+  ListBoxItem: TListBoxItem;
+  i: integer;
+begin
+  // true
+  if SwitchWifi.IsChecked then
+  begin
+    ListBoxItem := TListBoxItem.Create(ListBoxWifi);
+    ListBoxItem.Text := '无线网名称';
+    ListBoxItem.ItemData.Accessory := TListBoxItemData.TAccessory(2);
+    ListBoxItem.Height := 44;
+    ListBoxItem.StyleLookup := 'listboxitemrightdetail';
+
+    ListBoxWifi.AddObject(ListBoxItem);
+  end
+  else
+  // false
+  begin
+    for i := 0 to ListBoxWifi.ComponentCount - 1 do
+    begin
+      if ListBoxWifi.Components[i] is TListBoxItem then
+        if (ListBoxWifi.Components[i] as TListBoxItem).Text = '无线网名称' then
+          ListBoxWifi.RemoveObject((ListBoxWifi.Components[i] as TListBoxItem));
+    end;
+
+  end;
 end;
 
 end.
